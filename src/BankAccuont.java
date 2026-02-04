054cebb975c5f1c6b723390a386898984f574c1d
Juan Cossio <cossio0474@gmail.com>
2026-02-04 09:54:50 -0500

    Metodos consignar y retirar

--- src/BankAccuont.java
+++ src/BankAccuont.java
@@ -22,11 +22,26 @@ public class BankAccuont {
        return saldo;
    }
 
-   public void saetId(id)
-   {
-       this.id = id;
+   public void consignar(int valorAConstignar) throws IllegalAccessException {
+       if (valorAConstignar <= 0)
+       {
+           throw new IllegalAccessException("EL valor no es valido");
+       }
+       this.saldo += valorAConstignar;
    }
 
+public void retirar(int valorARetirar) throws IllegalAccessException {
+if (valorARetirar <= 0)
+{
+    throw new IllegalAccessException("el valor no es valido");
+}
 
+if (valorARetirar > saldo)
+{
+    throw new IllegalAccessException("el valor no es valdio");
+}
+
+this.saldo -= valorARetirar;
+}
 
 }
